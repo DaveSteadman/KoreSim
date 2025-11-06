7663c9759c99cdd6d1829623dcc0d401b5ca1629
Dave Steadman <davesteadman@me.com>
2025-11-06 20:34:01 +0000

    Update KoreCommandRuntime.cs

--- KoreCommon/CLI/Commands/KoreCommandRuntime.cs
+++ KoreCommon/CLI/Commands/KoreCommandRuntime.cs
@@ -13,33 +13,14 @@ public class KoreCommandRuntime : KoreCommand
         Signature.Add("runtime");
     }
 
-    //public override string HelpString => $"{SignatureString} <duration seconds>";
-
     public override string Execute(List<string> parameters)
     {
         StringBuilder sb = new();
 
-        // if (parameters.Count != 1)
-        // {
-        //     return "KoreCommandPause: Wrong number of parameters. Expected 1. Got " + parameters.Count;
-        // }
-
-        // if (!float.TryParse(parameters[0], out float duration))
-        // {
-        //     return "KoreCommandPause: Invalid duration. Please provide a valid number.";
-        // }
-
-        // Pause the console for the specified duration
-        // Thread.Sleep((int)(duration * 1000));
-
-
         sb.AppendLine($"Application runtime {KoreCentralTime.RuntimeSecs:F2} seconds");
-
-        // -------------------------------------------------
 
         return sb.ToString();
     }
-
 }
 
 
